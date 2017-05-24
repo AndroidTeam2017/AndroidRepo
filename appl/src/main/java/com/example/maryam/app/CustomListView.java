06ccdd29a9e6a52d0e211ae475c6cee4f76bc6cd
Maryam <mariamnazzari@gmail.com>
2017-05-24 11:13:25 +0430

    activity main changed register added

--- appl/src/main/java/com/example/maryam/app/CustomListView.java
+++ appl/src/main/java/com/example/maryam/app/CustomListView.java
@@ -7,28 +7,23 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
-import android.widget.TextView;
 
-public class Customlistviw extends ArrayAdapter<String> {
+public class CustomListView extends ArrayAdapter<String> {
     private String[] urls;
     private Bitmap[] bitmaps;
     private Activity context;
 
-    public Customlistviw(Activity context, String[] urls, Bitmap[] bitmaps) {
+    public CustomListView(Activity context, String[] urls, Bitmap[] bitmaps) {
         super(context, R.layout.image_list_view, urls);
         this.context = context;
         this.urls= urls;
         this.bitmaps= bitmaps;
     }
-
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         LayoutInflater inflater = context.getLayoutInflater();
         View listViewItem = inflater.inflate(R.layout.image_list_view, null, true);
-        TextView textViewURL = (TextView) listViewItem.findViewById(R.id.textViewURL);
-        ImageView image = (ImageView) listViewItem.findViewById(R.id.imageDownloaded);
-
-        textViewURL.setText(urls[position]);
+        ImageView image = (ImageView) listViewItem.findViewById(R.id.img);
         image.setImageBitmap(Bitmap.createScaledBitmap(bitmaps[position],100,50,false));
         return  listViewItem;
     }
